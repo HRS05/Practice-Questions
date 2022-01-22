5662e153e713e83f3f630802af6522fde6be22ef
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-01-22 09:53:07 +0530

    Time: 6 ms (30.50%), Space: 8.1 MB (29.03%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,7 +1,39 @@
 class Solution {
 public:
     
-    void check(vector<vector<int> > &ans,map<int,bool> &mp,vector<int> v,int index)
+    
+    void check(vector<vector<int> > &ans,vector<int> nums,int index)
+    {
+        if(index>=nums.size())
+        {
+            ans.push_back(nums);
+            return;
+        }
+        for(int i=index;i<nums.size();i++)
+        {
+            int g=nums[index];
+            nums[index]=nums[i];
+            nums[i]=g;
+            check(ans,nums,index+1);
+        }
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int> > ans;
+        vector<int> v;
+        map<int,bool> mp;
+        for(int it : nums){
+            mp[it]=false;
+        }
+        check(ans,nums,0);
+        return ans;
+    }
+    
+    
+    /*
+    //T : O(n! * n)
+    //S : O(n) + O(n)
+    void check(vector<vector<int> > &ans,map<int,bool> &mp,vector<int> v)
     {
         if(v.size()==mp.size())
         {
@@ -14,7 +46,7 @@ public:
             {
                 v.push_back(it->first);
                 it->second=true;
-                check(ans,mp,v,index);
+                check(ans,mp,v);
                 it->second=false;
                 v.pop_back();
             }
@@ -29,7 +61,8 @@ public:
         for(int it : nums){
             mp[it]=false;
         }
-        check(ans,mp,v,0);
+        check(ans,mp,v);
         return ans;
     }
+    */
 };
