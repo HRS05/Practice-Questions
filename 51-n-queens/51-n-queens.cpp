5d7bbb51fef67f409fc0414d2f7fabae9bad542f
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-01-22 22:19:24 +0530

    Time: 71 ms (13.83%), Space: 45.6 MB (15.60%) - LeetHub

---
+++ 51-n-queens/51-n-queens.cpp
@@ -0,0 +1,74 @@
+class Solution {
+public:
+    
+    bool isSafe(int row,int column,vector<string> board)
+    {
+        for(int i=0;i<board.size();i++)
+        {
+            if(board[i][column]=='Q' && i!=row) return false;
+            if(board[row][i]=='Q' && i!=column) return false;
+        }
+        int i,j;
+        i=row; j=column;
+        i=i-1; j=j+1;
+        while(i>=0 && i<board.size() && j>=0 && j<board.size())
+        {
+            if(board[i][j]=='Q') return false;
+            i=i-1; j=j+1;
+        }
+        i=row; j=column;
+        i=i+1; j=j-1;
+        while(i>=0 && i<board.size() && j>=0 && j<board.size())
+        {
+            if(board[i][j]=='Q') return false;
+            i=i+1; j=j-1;
+        }
+        i=row; j=column;
+        i=i+1; j=j+1;
+        while(i>=0 && i<board.size() && j>=0 && j<board.size())
+        {
+            if(board[i][j]=='Q') return false;
+            i=i+1; j=j+1;
+        }
+        i=row; j=column;
+        i=i-1; j=j-1;
+        while(i>=0 && i<board.size() && j>=0 && j<board.size())
+        {
+            if(board[i][j]=='Q') return false;
+            i=i-1; j=j-1;
+        }
+        return true;
+    }
+        
+    void solve(vector<vector<string> > &boards,vector<string> &board,int column)
+    {
+        if(column==board.size())
+        {
+            boards.push_back(board);
+            return;
+        }
+        for(int row=0;row<board.size();row++)
+        {
+            if(isSafe(row,column,board))
+            {
+                board[row][column]='Q';
+                solve(boards,board,column+1);
+                board[row][column]='.';
+            }
+        }
+        
+    }
+    
+    
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string> > boards;
+        vector<string> board;
+        string file(n,'.');
+        for(int i=0;i<n;i++)
+        {
+            board.push_back(file);
+        }
+        solve(boards,board,0);
+        return boards;    
+    }
+};
