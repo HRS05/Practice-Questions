983441fb0d564562837def5e3a7943deabc93fb3
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-01-23 09:05:44 +0530

    Time: 7 ms (69.54%), Space: 7.3 MB (69.74%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,6 +1,50 @@
 class Solution {
 public:
     
+    
+    void solve(vector<vector<string> > &boards,vector<string> &board,int column,vector<int> &leftRow,vector<int> &upperDiagonal,vector<int> &lowerDiagonal)
+    {
+        if(column==board.size())
+        {
+            boards.push_back(board);
+            return;
+        }
+        for(int row=0;row<board.size();row++)
+        {
+            if(leftRow[row]==0 && lowerDiagonal[row+column]==0 && upperDiagonal[board.size()-1+column-row]==0)
+            {
+                board[row][column]='Q';
+                leftRow[row]=1;
+                lowerDiagonal[row+column]=1;
+                upperDiagonal[board.size()-1+column-row]=1;
+                solve(boards,board,column+1,leftRow,upperDiagonal,lowerDiagonal);
+                board[row][column]='.';
+                leftRow[row]=0;
+                lowerDiagonal[row+column]=0;
+                upperDiagonal[board.size()-1+column-row]=0;
+            }
+        }
+        
+    }
+    
+    
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string> > boards;
+        vector<string> board;
+        vector<int> leftRow(n,0),upperDiagonal(2 * n-1,0),lowerDiagonal(2 * n-1,0);
+        string file(n,'.');
+        for(int i=0;i<n;i++)
+        {
+            board.push_back(file);
+        }
+        solve(boards,board,0,leftRow,upperDiagonal,lowerDiagonal);
+        return boards;    
+    }
+    
+    
+    
+    /*
+    
     bool isSafe(int row,int column,vector<string> board)
     {
         for(int i=column-1;i>=0;i--)
@@ -57,4 +101,6 @@ public:
         solve(boards,board,0);
         return boards;    
     }
+    
+    */
 };
