64bf2a4b99094c88386761ff3969001cce4043ae
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-01-29 11:04:59 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (82.28%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,7 +1,21 @@
 class Solution {
 public:
-    int dp[45]={-1};
+    
     int climbStairs(int n) {
+        //fibonacci by observation
+        int x,y,num;
+        x=0;
+        y=1;
+        for(int i=1;i<=n;i++)
+        {
+            num=x+y;
+            x=y;
+            y=num;
+        }
+        return num;
+        /*
+        //dp approach
+        int dp[45]={-1};
         dp[0]=1;
         dp[1]=2;
         for(int i=2;i<n;i++)
@@ -9,5 +23,6 @@ public:
             dp[i]=dp[i-1]+dp[i-2];
         }
         return dp[n-1];
+        */
     }
 };
