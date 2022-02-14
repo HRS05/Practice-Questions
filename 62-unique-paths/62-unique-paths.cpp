c88a456353d7002b970639ac669599cfcb466509
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-02-14 15:08:20 +0530

    Time: 3 ms (30.88%), Space: 6.5 MB (40.71%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -5,7 +5,6 @@ public:
     {
        
         if(m<=0 || n<=0) return 0;
-        if(n==1 && m==1) return 1;
         if(dp[m][n]!=-1) return dp[m][n];
         int right,down;
         dp[m-1][n]=right=uni(m-1,n,dp);
@@ -13,6 +12,26 @@ public:
         return dp[m][n]=right+down;
     }
     
+    int uniquePaths(int m, int n) {
+         vector<vector<int> > dp(m+1,vector<int>(n+1,-1));
+        dp[0][0]=0;
+        dp[1][1]=1;
+      
+        return uni(m,n,dp);
+    }
+    
+    /*
+    //memoized code
+    int uni(int m,int n,vector<vector<int> > &dp)
+    {
+       
+        if(m<=0 || n<=0) return 0;
+        if(dp[m][n]!=-1) return dp[m][n];
+        int right,down;
+        dp[m-1][n]=right=uni(m-1,n,dp);
+        dp[m][n-1]=down=uni(m,n-1,dp);
+        return dp[m][n]=right+down;
+    }
     
     int uniquePaths(int m, int n) {
          vector<vector<int> > dp(m+1,vector<int>(n+1,-1));
@@ -20,4 +39,5 @@ public:
         dp[1][1]=1;
         return uni(m,n,dp);
     }
+    */
 };
