426ad276e2581136fe7f6c69da1685139aa34979
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-02-14 15:49:33 +0530

    Time: 3 ms (30.88%), Space: 6.5 MB (23.94%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     
-    
     int uni(int m,int n,vector<vector<int> > &dp)
     {
        
@@ -9,8 +8,41 @@ public:
         if(n==1 && m==1) return 1;
         if(dp[m][n]!=-1) return dp[m][n];
         int right,down;
-        dp[m-1][n]=right=uni(m-1,n,dp);
-        dp[m][n-1]=down=uni(m,n-1,dp);
+        right=uni(m-1,n,dp);
+        down=uni(m,n-1,dp);
+        return dp[m][n]=right+down;
+    }
+    
+    
+    int uniquePaths(int m, int n) {
+         vector<vector<int> > dp(m+1,vector<int>(n+1,0));
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(i==1 && j==1) dp[i][j]=1;
+                else
+                {
+                    dp[i][j]=dp[i][j-1]+dp[i-1][j];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+    
+    
+    
+    /*
+    //memoized code
+    int uni(int m,int n,vector<vector<int> > &dp)
+    {
+       
+        if(m<=0 || n<=0) return 0;
+        if(n==1 && m==1) return 1;
+        if(dp[m][n]!=-1) return dp[m][n];
+        int right,down;
+        right=uni(m-1,n,dp);
+        down=uni(m,n-1,dp);
         return dp[m][n]=right+down;
     }
     
@@ -21,4 +53,5 @@ public:
         dp[1][1]=1;
         return uni(m,n,dp);
     }
+    */
 };
