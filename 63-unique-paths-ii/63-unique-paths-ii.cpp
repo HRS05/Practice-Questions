e86c182f89ced68398c24e139ddbd2d82bd043ea
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-02-14 21:36:33 +0530

    Time: 10 ms (14.26%), Space: 8 MB (11.62%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,7 +1,33 @@
 class Solution {
 public:
     
+    //tabulation code
+   
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m=obstacleGrid.size();
+        int n=obstacleGrid[0].size();
+        vector<vector<int> > dp(m,vector<int>(n,0));
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(obstacleGrid[i][j]==1) 
+                {    
+                    dp[i][j]=0;
+                    continue;
+                }
+                if(i==0 && j==0) dp[i][j]=1;
+                else
+                {
+                    dp[i][j]=( j>0 ? dp[i][j-1] : 0 )+( i>0 ? dp[i-1][j] : 0);
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+    
     //memoized code
+    /*
     int uni(int m,int n,vector<vector<int> > &dp,vector<vector<int>>& obstacleGrid)
     {
         if(m>=0 && n>=0 && obstacleGrid[m][n]==1) return 0;
@@ -19,4 +45,5 @@ public:
         dp[0][0]=1;
         return uni(obstacleGrid.size()-1,obstacleGrid[0].size()-1,dp,obstacleGrid);
     }
+    */
 };
