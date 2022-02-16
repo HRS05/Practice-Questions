435aacfced3418fe21e04c7e057525b67b88916a
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-02-16 17:49:28 +0530

    Time: 136 ms (18.97%), Space: 14.5 MB (25.74%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,6 +1,60 @@
 class Solution {
 public:
     
+    //tabulation 
+    int cherryPickup(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int> > rdp(m,vector<int>(m,0));
+        vector<vector<vector<int> > > dp(n,rdp);
+        
+        //initia..
+        for(int j1=0;j1<m;j1++)
+        {
+            for(int j2=0;j2<m;j2++)
+            {
+                if(j1==j2) dp[n-1][j1][j2]=grid[n-1][j1];
+                else dp[n-1][j1][j2]=grid[n-1][j1]+grid[n-1][j2];
+            }
+        }
+        
+        
+        for(int i=n-2;i>=0;i--)
+        {
+            for(int j1=0;j1<m;j1++)
+            {
+                for(int j2=0;j2<m;j2++)
+                {
+                    int maxi=-1e8;
+                    for(int dj1=-1;dj1<=1;dj1++)
+                    {
+                        for(int dj2=-1;dj2<=1;dj2++)
+                        {
+                            int value=0;
+                            if(j1==j2)
+                                value=grid[i][j1];
+                            else
+                                value=grid[i][j1]+grid[i][j2];
+                            if(j1+dj1<0 || j1+dj1>=m || j2+dj2<0 || j2+dj2>=m) 
+                                value=-1e8;
+                            else 
+                                value+=dp[i+1][j1+dj1][j2+dj2];
+                            maxi=max(maxi,value);
+                        }
+                    }
+                    dp[i][j1][j2]=maxi;
+                }
+            }
+        }
+        return dp[0][0][m-1];
+        
+    }
+    
+    
+    /*
+    //memoized code
+    //TC : O(n*m*m*9)
+    //SC : O(n)+O(n*m*m)
     int picking(vector<vector<int> > &grid,int i,int j1,int j2,vector<vector<vector<int>>> &dp)
     {
         int n=grid.size();
@@ -37,4 +91,38 @@ public:
         vector<vector<vector<int> > > dp(n,rdp);
         return picking(grid,0,0,grid[0].size()-1,dp); 
     }
+    */
+    
+    
+    /*
+    //recursive code
+    //TC : O(3^n * 3^n)
+    //SC : O(n)
+     int picking(vector<vector<int> > &grid,int i,int j1,int j2)
+    {
+        int n=grid.size();
+        int m=grid[0].size();
+        if(j1>=m || j2>=m || j1<0 || j2<0) return -1e8;
+        if(i==n-1)
+        {
+            if(j1==j2) return grid[i][j1];
+            else return grid[i][j1]+grid[i][j2];
+        }
+        int maxi=INT_MIN;
+        for(int dj1=-1;dj1<=1;dj1++)
+        {
+            for(int dj2=-1;dj2<=1;dj2++)
+            {
+                if(j1==j2) maxi=max(maxi,grid[i][j1]+picking(grid,i+1,j1+dj1,j2+dj2));
+                else maxi=max(maxi,grid[i][j1]+grid[i][j2]+picking(grid,i+1,j1+dj1,j2+dj2));
+            }
+        }
+        return maxi;    
+    }
+    
+    int cherryPickup(vector<vector<int>>& grid) {
+        return picking(grid,0,0,grid[0].size()-1); 
+    }
+    */
+    
 };
