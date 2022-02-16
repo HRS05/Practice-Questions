0f69173fed1fac94508b7a48ff8911fa74892ee9
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-02-16 17:55:37 +0530

    Time: 120 ms (22.71%), Space: 14.4 MB (38.10%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,7 +1,65 @@
 class Solution {
 public:
     
-    //tabulation 
+    
+    //space optimize
+    //TC : O(n*m*m*9)
+    //SC : O(m*m)
+    int cherryPickup(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int> > prev(m,vector<int>(m,0));
+        
+        
+        //initia..
+        for(int j1=0;j1<m;j1++)
+        {
+            for(int j2=0;j2<m;j2++)
+            {
+                if(j1==j2) prev[j1][j2]=grid[n-1][j1];
+                else prev[j1][j2]=grid[n-1][j1]+grid[n-1][j2];
+            }
+        }
+        
+        
+        for(int i=n-2;i>=0;i--)
+        {
+            vector<vector<int> > curr(m,vector<int>(m,0));
+            for(int j1=0;j1<m;j1++)
+            {
+                for(int j2=0;j2<m;j2++)
+                {
+                    int maxi=-1e8;
+                    for(int dj1=-1;dj1<=1;dj1++)
+                    {
+                        for(int dj2=-1;dj2<=1;dj2++)
+                        {
+                            int value=0;
+                            if(j1==j2)
+                                value=grid[i][j1];
+                            else
+                                value=grid[i][j1]+grid[i][j2];
+                            if(j1+dj1<0 || j1+dj1>=m || j2+dj2<0 || j2+dj2>=m) 
+                                value=-1e8;
+                            else 
+                                value+=prev[j1+dj1][j2+dj2];
+                            maxi=max(maxi,value);
+                        }
+                    }
+                    curr[j1][j2]=maxi;
+                }
+            }
+            prev=curr;
+        }
+        return prev[0][m-1];
+    }
+    
+    
+    
+    /*
+    //tabulation
+    //TC : O(n*m*m*9)
+    //SC : O(n*m*m)
     int cherryPickup(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
@@ -47,9 +105,8 @@ public:
             }
         }
         return dp[0][0][m-1];
-        
     }
-    
+    */
     
     /*
     //memoized code
