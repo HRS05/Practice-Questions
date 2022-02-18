bdbddc89cb80ccdb254e84ac5a00ebd96768c75f
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-02-18 17:37:51 +0530

    Time: 18 ms (75.11%), Space: 16.4 MB (68.91%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     
+    int jump(vector<int>& nums) {
+        int n=nums.size();
+        if(n<=1) return 0;
+        if(nums[0]==0) return -1;
+        int maxReach=nums[0];
+        int step=nums[0];
+        int jump=1;
+        
+        for(int i=1;i<n;i++)
+        {
+            if(i==n-1) return jump;
+            maxReach=max(maxReach,nums[i]+i);
+            step--;
+            if(step==0)
+            {
+                jump++;
+                if(i>=maxReach) return -1;
+                step=maxReach-i;
+            }
+        }
+        return jump;
+    }
+    
+    
+    /*
+    memoized code
     int result(vector<int> &nums,int maxJumps,int index,vector<int> &dp)
     {
         if(index==nums.size()-1) return 0;
@@ -20,5 +46,5 @@ public:
     int jump(vector<int>& nums) {
         vector<int> dp(nums.size(),-1);
         return result(nums,nums[0],0,dp);
-    }
+    }*/
 };
