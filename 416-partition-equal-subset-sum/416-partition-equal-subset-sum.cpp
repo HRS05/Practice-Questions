ace72f6a7675c0827b30fb539b0bd55bc2b8becd
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-02-19 09:46:30 +0530

    Time: 461 ms (16.46%), Space: 75.4 MB (17.97%) - LeetHub

---
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    
+    
+    //memoized code
+    bool checkTarget(vector<int> &nums,int index,int target,vector<vector<int> > &dp)
+    {
+        if(index<0 || target<0) return false;
+        if(index==0) return (nums[0]==target);
+        if(dp[index][target]!=-1) return dp[index][target];
+        bool notTake=checkTarget(nums,index-1,target,dp);
+        bool take=checkTarget(nums,index-1,target-nums[index],dp);
+        return dp[index][target]=take || notTake; 
+    }
+    
+    bool canPartition(vector<int>& nums) 
+    {
+        int totalSum=0;
+        for(int i=0;i<nums.size();i++) totalSum+=nums[i];
+        if(totalSum%2!=0) return false;
+        totalSum=totalSum/2;
+        
+        vector<vector<int> > dp(nums.size(),vector<int>(totalSum+1,-1));
+        
+        return checkTarget(nums,nums.size()-1,totalSum,dp);
+        
+    }
+    
+    
+    
+    /*
+    //recursive code
+    //TC : O(2^n)
+    bool checkTarget(vector<int> &nums,int index,int target)
+    {
+        if(index<0) return false;
+        if(nums[index]==target) return true;
+        bool notTake=checkTarget(nums,index-1,target);
+        bool take=checkTarget(nums,index-1,target-nums[index]);
+        return take || notTake; 
+    }
+    
+    bool canPartition(vector<int>& nums) 
+    {
+        int totalSum=0;
+        for(int i=0;i<nums.size();i++) totalSum+=nums[i];
+        if(totalSum%2!=0) return false;
+        totalSum=totalSum/2;
+        return checkTarget(nums,nums.size()-1,totalSum);
+        
+    }
+    */
+};
