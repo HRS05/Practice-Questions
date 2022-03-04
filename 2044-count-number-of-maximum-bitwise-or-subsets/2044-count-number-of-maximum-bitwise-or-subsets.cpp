bbee3aa6d83baa5926bc238d6d6f92b668c30257
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-03-04 10:06:00 +0530

    Time: 46 ms (63.75%), Space: 8.4 MB (87.49%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     
-   int subset(vector<int> &nums, int i, int a,int b)
-{
-    int ans =0;
-    if(i<0)
-        return 0;
-    if(a == (b|nums[i]))
-        ans =1;
-    return ans + subset(nums,i-1,a,b) + subset(nums,i-1,a,b|nums[i]);
-}
-
-int countMaxOrSubsets(vector<int>& nums) {
-    
-    int a=0;
-    
-    for(auto i:nums)
+    int f(vector<int> &nums,int result,int cal,int index)
     {
-        a = a|i;
+        int res=0;
+        if(index<0) return 0;
+        if(result == (cal | nums[index]))
+        {
+            res++;
+        }
+        int take=f(nums,result,cal|nums[index],index-1);
+        int notTake=f(nums,result,cal,index-1);
+        return res+take+notTake;
     }
     
-    int ans = subset(nums,nums.size()-1,a,0);
-        
-    return ans ;
     
-}
+    int countMaxOrSubsets(vector<int>& nums) {
+        int result=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            result=result | nums[i];
+        }
+        
+        return f(nums,result,0,nums.size()-1);
+    }
 };
