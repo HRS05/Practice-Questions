68fdeef496530186cf5365be2c1ef6b0e4e337e2
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-03-23 22:57:43 +0530

    Time: 89 ms (48.67%), Space: 18 MB (61.42%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -1,5 +1,30 @@
 class Solution {
 public:
+    
+    //space optimization
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int rows=matrix.size();
+        int columns=matrix[0].size();
+        vector<int> prev(columns+1,0);
+        int maxSquareLength=0;
+        for(int i=1;i<=rows;i++)
+        {
+            vector<int> curr(columns+1,0);
+            for(int j=1;j<=columns;j++)
+            {
+                if(matrix[i-1][j-1]=='1')
+                {
+                    curr[j]=min(min(curr[j-1],prev[j]) , prev[j-1])+1;
+                    maxSquareLength=max(maxSquareLength,curr[j]);
+                }
+            }
+            prev=curr;
+        }
+        return maxSquareLength * maxSquareLength;
+    }
+    
+    
+    /*
     int maximalSquare(vector<vector<char>>& matrix) {
         int rows=matrix.size();
         int columns=matrix[0].size();
@@ -18,4 +43,5 @@ public:
         }
         return maxSquareLength * maxSquareLength;
     }
+    */
 };
