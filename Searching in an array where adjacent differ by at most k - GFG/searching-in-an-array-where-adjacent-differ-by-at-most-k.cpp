fefed1d2a4db407c0d720d192530321a4fd8033c
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-03-29 15:52:30 +0530

    Added solution - LeetHub

---
+++ Searching in an array where adjacent differ by at most k - GFG/searching-in-an-array-where-adjacent-differ-by-at-most-k.cpp
@@ -0,0 +1,56 @@
+// { Driver Code Starts
+// C++ program to search an element in an array
+// where difference between all elements is 1
+#include<bits/stdc++.h>
+using namespace std;
+
+// x is the element to be searched in arr[0..n-1]
+// such that all elements differ by at-most k.
+int search(int arr[], int n, int x, int k);
+
+
+// Driver program to test above function
+int main()
+{
+	int t,n,k,x;
+	cin>>t;
+	while(t--)
+    {
+
+        cin>>n>>k;
+        int arr[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        cin>>x;
+        cout<<search(arr, n, x, k)<<endl;
+
+    }
+return 0;
+}
+
+// } Driver Code Ends
+
+
+// x is the element to be searched in arr[0..n-1]
+// such that all elements differ by at-most k.
+int search(int arr[], int n, int x, int k)
+{
+    if(n==1 && arr[0]==x) return 0;
+    
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]==x)
+        {
+            if(i==0)
+            {
+                if(abs(arr[i+1]-arr[i])<=k) return i;
+            }
+            else if(i==n-1)
+            {
+                if(abs(arr[i-1]-arr[i])<=k) return i;
+            }
+            if(abs(arr[i+1]-arr[i])<=k && abs(arr[i-1]-arr[i])<=k) return i;
+        }
+    }
+    return -1;
+}    	
