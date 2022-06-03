ce16c18a23d12b66add67574918e32e2f333e104
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-06-03 16:12:46 +0530

    Time: 1056 ms (5.02%), Space: 151 MB (13.84%) - LeetHub

---
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
@@ -0,0 +1,90 @@
+class NumMatrix {
+    
+    
+private:
+    vector<vector<int> > matrix;
+public:
+    NumMatrix(vector<vector<int>>& matrix) {
+        vector<vector<int> > dp(matrix.size(),vector<int>(matrix[0].size(),0));
+        
+        for(int i=0;i<matrix.size();i++)
+        {
+            for(int j=0;j<matrix[0].size();j++)
+            {
+                if(j==0) dp[i][j]=matrix[i][j];
+                else dp[i][j]=dp[i][j-1]+matrix[i][j];
+            }
+        }
+        
+        for(int i=0;i<matrix.size();i++)
+        {
+            for(int j=0;j<matrix[0].size();j++)
+            {
+                cout<<dp[i][j]<<"  ";
+            }
+            cout<<endl;
+        }
+        this->matrix=dp;
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        int ans=0;
+        if(row1==row2 && col1==col2) {
+            if(col1==0)
+            {
+                return this->matrix[row1][col1];
+            }
+            else
+            {
+                return this->matrix[row2][col2]-this->matrix[row2][col2-1];
+            }
+        }
+        
+        for(int i=row1;i<=row2;i++)
+        {
+            if(col1!=0) ans+=this->matrix[i][col2]-this->matrix[i][col1-1];
+            else ans+=this->matrix[i][col2];
+        }
+        return ans;
+    }
+        
+
+    /*
+    // TLE solution
+    
+private:
+    vector<vector<int> > matrix;
+public:
+    NumMatrix(vector<vector<int>>& matrix) {
+        for(int i=0;i<matrix.size();i++)
+        {
+            vector<int> v;
+            for(int j=0;j<matrix[0].size();j++)
+            {
+                v.push_back(matrix[i][j]);
+            }
+            this->matrix.push_back(v);
+        }
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+       int ans=0;
+        for(int i=row1;i<=row2 && i>=0;i++)
+        {
+           for(int j=col1;j<=col2 && j>=0;j++)
+           {
+               ans=ans+this->matrix[i][j];
+               //cout<<matrix[i][j]<<"  ";
+           }
+            cout<<endl;
+        }
+        return ans;
+    }
+    */
+};
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ */
