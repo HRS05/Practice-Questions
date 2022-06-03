98dd122282e3ae928755262ffec8a00a34cf35cc
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-06-03 16:42:47 +0530

    Time: 457 ms (82.92%), Space: 148 MB (48.25%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
@@ -1,6 +1,31 @@
 class NumMatrix {
+
+private:
+    vector<vector<int> > matrix;
+public:
+    NumMatrix(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        matrix=vector<vector<int> >(n+1,vector<int>(m+1,0));
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                matrix[i][j]=matrix[i-1][j]+matrix[i][j-1]-matrix[i-1][j-1]+mat[i-1][j-1];
+            }
+        }
+        
+    }
+
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        row1++; col1++; row2++; col2++; 
+        return matrix[row2][col2] - matrix[row2][col1 - 1] - matrix[row1 - 1][col2] + matrix[row1 - 1][col1 - 1];
+    }
     
     
+/*    
+// sc: O(m*n)
+// TC : O(r2-r1)
 private:
     vector<vector<int> > matrix;
 public:
@@ -28,7 +53,7 @@ public:
         }
         return ans;
     }
-        
+     */   
 
     /*
     // TLE solution
