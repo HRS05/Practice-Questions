3a8f3dceccc18507ada0a013d43ae14e89a9f03e
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-08-08 14:11:43 +0530

    Time: 170 ms (78.93%), Space: 71.8 MB (84.37%) - LeetHub

--- 687-longest-univalue-path/687-longest-univalue-path.cpp
+++ 687-longest-univalue-path/687-longest-univalue-path.cpp
@@ -11,6 +11,20 @@
  */
 class Solution {
 public:
+    int ans{0};
+    int helper(TreeNode *root,int val){
+        if(root==NULL) return 0;
+        int lh=helper(root->left,root->val);
+        int rh=helper(root->right,root->val);
+        ans=max(ans,(lh+rh));
+        return val==root->val ? 1+max(lh,rh) : 0;
+    }
+    int longestUnivaluePath(TreeNode* root) {
+        helper(root,-10001);
+        return ans;
+    }
+    
+    /*
     int longestUnivaluePath(TreeNode* root) {
         if(root==NULL) return 0;
         int lCal=helper(root->left,root->val);
@@ -26,4 +40,5 @@ public:
         if(root->val != val) return 0;
         return 1+max(helper(root->left,root->val),helper(root->right,root->val));
     }
+    */
 };
