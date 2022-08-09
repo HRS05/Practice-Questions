0e2b98fc9353f6ca39315bf5892791d3cb80fa57
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-08-09 11:09:26 +0530

    Time: 13 ms (61.99%), Space: 17.4 MB (19.97%) - LeetHub

---
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -0,0 +1,75 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    int widthOfBinaryTree(TreeNode* root) {
+        unsigned long long ans{0};
+        queue<pair<TreeNode *,unsigned long long> > q;
+        q.push({root,0});
+        while(!q.empty())
+        {
+            unsigned long long size=q.size();
+            unsigned long long index_level=q.front().second;
+            unsigned long long index=q.front().second;
+            for(unsigned long long i=0;i<size;i++)
+            {
+                TreeNode *node=q.front().first;
+                index=q.front().second;
+                q.pop();
+                if(node->left) q.push({node->left,2*index});
+                if(node->right) q.push({node->right,2*index+1});
+            } 
+            ans=max(ans,(index-index_level));
+        }
+        return ans+1;
+    }
+    
+    
+    /*
+    int widthOfBinaryTree(TreeNode* root) {
+        int ans=0;
+        queue<TreeNode *> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            int size=q.size();
+            int count=0;
+            int lastCount;
+            int f=-1,l=-1;
+            bool first=true;
+            for(int i=0;i<size;i++)
+            {
+                TreeNode *node=q.front(); q.pop();
+                if(node) {
+                    q.push(node->left);
+                    q.push(node->right);
+                }else{
+                    q.push(NULL);
+                    q.push(NULL);
+                }
+                if(node && first)
+                {
+                    f=i;
+                    first=false;
+                }
+                if(node)
+                {
+                    l=i;
+                }
+            }
+            if(l==-1 && f==-1) break;
+            ans=max(ans,(l-f));
+        }   
+        return ans+1;
+    }*/
+};
