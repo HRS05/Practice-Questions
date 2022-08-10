0b7701ee3b83745258243711a19f58932639ef43
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-08-11 00:14:15 +0530

    Time: 20 ms (49.95%), Space: 21.7 MB (30.71%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -12,32 +12,20 @@
 class Solution {
 public:
     
-    bool isValidBST(TreeNode* root) {
-       TreeNode *prev=NULL;
-        return validate(root,prev);
-    }
     
-    bool validate(TreeNode *node,TreeNode *&prev)
+    
+    
+    bool helper(TreeNode *root,TreeNode *&prev)
     {
-        if(node==NULL) return true;
-        if(!validate(node->left,prev)) return false;
-        if(prev!=NULL && prev->val >= node->val) return false;
-        prev=node;
-        return validate(node->right,prev);
+        if(root==NULL) return true;
+        if(!helper(root->left,prev)) return false;
+        if(prev && prev->val >= root->val) return false;
+        prev=root;
+        return helper(root->right,prev);
+    }   
+    
+    bool isValidBST(TreeNode* root) {
+        TreeNode *prev=NULL;
+        return helper(root,prev);
     }
-    
-    
-//     bool helper(TreeNode *root,TreeNode *prev)
-//     {
-//         if(root==NULL) return true;
-//         if(!helper(root->left,prev)) return false;
-//         if(prev && prev->val >= root->val) return false;
-//         prev=root;
-//         return helper(root->right,prev);
-//     }   
-    
-//     bool isValidBST(TreeNode* root) {
-//         TreeNode *prev=NULL;
-//         return helper(root,prev);
-//     }
 };
