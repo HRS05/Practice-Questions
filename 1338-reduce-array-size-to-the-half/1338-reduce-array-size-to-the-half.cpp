6bf4f1f9dba209b6c5ac8be163cb18a67c4f8db6
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-08-18 13:50:21 +0530

    Time: 154 ms (99.21%), Space: 57.9 MB (96.94%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -3,27 +3,27 @@ public:
     int minSetSize(vector<int>& arr) {
         sort(arr.begin(),arr.end());
         int n=arr.size();
-        priority_queue<pair<int,int> > q;
+        priority_queue<int> q;
         int prev=0;
         int cnt=0;
         for(int i=0;i<n;i++)
         {
             cnt++;
             if(i+1<n && arr[i+1]!=arr[i]) {
-                q.push({cnt,arr[i]});
+                q.push(cnt);
                 cnt=0;
             }else if(i+1==n) {
-                q.push({cnt,arr[i]});
+                q.push(cnt);
             }
         }
         int count=arr.size();
         int ans=0;
         while(!q.empty() && count>arr.size()/2)
         {
-            pair<int,int> p=q.top();
+            int p=q.top();
             q.pop();
             ans++;
-            count-=p.first;
+            count-=p;
             //cout<<p.first<<"  "<<p.second<<endl;
         }
         return ans;
