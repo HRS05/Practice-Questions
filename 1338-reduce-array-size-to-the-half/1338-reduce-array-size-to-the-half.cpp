323aeac7edb09b890e79074ff977244784bcd19d
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-08-18 13:46:59 +0530

    Time: 291 ms (68.64%), Space: 60.7 MB (92.92%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -1,6 +1,37 @@
 class Solution {
 public:
     int minSetSize(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+        int n=arr.size();
+        priority_queue<pair<int,int> > q;
+        int prev=0;
+        int cnt=0;
+        for(int i=0;i<n;i++)
+        {
+            cnt++;
+            if(i+1<n && arr[i+1]!=arr[i]) {
+                q.push({cnt,arr[i]});
+                cnt=0;
+            }if(i+1==n) {
+                q.push({cnt,arr[i]});
+            }
+        }
+        int count=arr.size();
+        int ans=0;
+        while(!q.empty() && count>arr.size()/2)
+        {
+            pair<int,int> p=q.top();
+            q.pop();
+            ans++;
+            count-=p.first;
+            //cout<<p.first<<"  "<<p.second<<endl;
+        }
+        return ans;
+    }
+    
+    
+    /*
+    int minSetSize(vector<int>& arr) {
         map<int,int> mp;
         for(int num : arr)
         {
@@ -22,5 +53,5 @@ public:
             //cout<<p.first<<"  "<<p.second<<endl;
         }
         return ans;
-    }
+    }*/
 };
