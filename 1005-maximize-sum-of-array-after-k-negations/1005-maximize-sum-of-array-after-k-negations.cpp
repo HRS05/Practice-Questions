04046dc2477fd51486de57b304f1876ae99c9d0e
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-08-26 09:27:54 +0530

    Time: 8 ms (65.61%), Space: 9.2 MB (10.88%) - LeetHub

--- 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.cpp
+++ 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.cpp
@@ -1,19 +1,45 @@
 class Solution {
 public:
     int largestSumAfterKNegations(vector<int>& nums, int k) {
-        sort(nums.begin(),nums.end());
+        priority_queue<int,vector<int>,greater<int>> pq;
         int sum=0;
         for(int i=0;i<nums.size();i++)
         {
-            if(k>0 && nums[i]<0){
-                nums[i]*=-1;
-                k--;
-            }
+            pq.push(nums[i]);
             sum+=nums[i];
         }
-        if(k==0 || k%2==0) return sum;
-        sort(nums.begin(),nums.end());
-       
-        return sum-(2*nums[0]);
+        while(k)
+        {
+            int val=pq.top();
+            if(val==0){
+                k--;
+                continue;
+            }
+            pq.pop();
+            val*=-1;
+            sum+=(2*val);
+            k--;
+            pq.push(val);
+        }
+        return sum;
+        
     }
+    
+    
+//     int largestSumAfterKNegations(vector<int>& nums, int k) {
+//         sort(nums.begin(),nums.end());
+//         int sum=0;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             if(k>0 && nums[i]<0){
+//                 nums[i]*=-1;
+//                 k--;
+//             }
+//             sum+=nums[i];
+//         }
+//         if(k==0 || k%2==0) return sum;
+//         sort(nums.begin(),nums.end());
+       
+//         return sum-(2*nums[0]);
+//     }
 };
