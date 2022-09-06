d6c04a77ca4b28d32a71532cc0c27ef491aace0d
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-09-06 16:11:34 +0530

    Time: 16 ms (61.96%), Space: 13.1 MB (27.73%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,6 +1,37 @@
 class Solution {
 public:
     
+    //tabulation code
+    int maxProfit(int k,vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<vector<int> > > dp(n+1,vector<vector<int>>(2,vector<int>(k+1,0)));
+        for(int index=n-1;index>=0;index--)
+        {
+            for(int buy=0;buy<=1;buy++)
+            {
+                for(int cap=1;cap<=k;cap++)
+                {
+                     if(buy)
+                    {
+                        dp[index][buy][cap]=max((-prices[index]+dp[index+1][0][cap]),dp[index+1][1][cap]);
+                        //dp[index][buy][cap]=max((-prices[index]+helper(prices,index+1,0,cap,dp)),helper(prices,index+1,1,cap,dp));
+                    }
+                    else
+                    {
+                        dp[index][buy][cap]=max((prices[index]+dp[index+1][1][cap-1]),dp[index+1][0][cap]);
+                        //dp[index][buy][cap]=max((prices[index]+helper(prices,index+1,1,cap-1,dp)),helper(prices,index+1,0,cap,dp));
+                    }
+                    
+                }
+            }
+        }
+        
+        return dp[0][1][k];
+    }
+    
+    
+    
+    /*
     int helper(vector<int> &prices,int index,int buy,int cap,vector<vector<vector<int> > > &dp)
     {
         int n=prices.size();
@@ -24,4 +55,5 @@ public:
         vector<vector<vector<int> > > dp(n,vector<vector<int>>(2,vector<int>(k+1,-1)));
         return helper(prices,0,1,k,dp);
     }
+    */
 };
