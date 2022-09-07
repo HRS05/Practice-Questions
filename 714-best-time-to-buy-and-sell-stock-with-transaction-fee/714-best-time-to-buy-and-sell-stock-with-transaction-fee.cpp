aa14bb3c6a4f77827ba4063841765f54304ecaea
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-09-07 14:29:38 +0530

    Time: 158 ms (78.03%), Space: 55.1 MB (76.89%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,5 +1,38 @@
 class Solution {
 public:
+    
+    //tabulation code and space optimization
+    int maxProfit(vector<int>& prices, int fee) 
+    {
+        int n = prices.size();
+        //sell=0 // buy=1
+        vector<int> after(2,0);
+        vector<int> curr(2,0);
+        
+        for(int index=n-1;index>=0;index--)
+        {
+            for(int action=0;action<2;action++)
+            {
+                if(action==1)
+                {
+                    curr[action]=max((-prices[index]+after[0]),after[action]);
+                    //ans=max((-prices[index]+helper(index+1,0,fee,prices,dp)),helper(index+1,action,fee,prices,dp));
+                }
+                else
+                {
+                    curr[action]=max((prices[index]+after[1]-fee),after[action]);
+                    //ans=max((prices[index]+helper(index+1,1,fee,prices,dp)-fee),helper(index+1,action,fee,prices,dp));
+                }   
+            }
+            after=curr;
+        }
+        
+        return after[1];
+    }
+    
+    
+    
+    /*
     //tabulation code
     int maxProfit(vector<int>& prices, int fee) 
     {
@@ -25,6 +58,8 @@ public:
         
         return dp[0][1];
     }
+    */
+    
     
     /*
     //recursive and memoized solution
