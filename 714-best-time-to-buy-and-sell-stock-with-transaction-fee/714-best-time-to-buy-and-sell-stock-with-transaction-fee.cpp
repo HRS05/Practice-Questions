d25b94e6a0434f2f88917a32e89f1d31b2d8723c
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-09-07 14:27:03 +0530

    Time: 562 ms (5.01%), Space: 90.8 MB (45.07%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,6 +1,33 @@
 class Solution {
 public:
+    //tabulation code
+    int maxProfit(vector<int>& prices, int fee) 
+    {
+        int n = prices.size();
+        //sell=0 // buy=1
+        vector<vector<int> > dp(n+1,vector<int>(2,0));
+        for(int index=n-1;index>=0;index--)
+        {
+            for(int action=0;action<2;action++)
+            {
+                if(action==1)
+                {
+                    dp[index][action]=max((-prices[index]+dp[index+1][0]),dp[index+1][action]);
+                    //ans=max((-prices[index]+helper(index+1,0,fee,prices,dp)),helper(index+1,action,fee,prices,dp));
+                }
+                else
+                {
+                    dp[index][action]=max((prices[index]+dp[index+1][1]-fee),dp[index+1][action]);
+                    //ans=max((prices[index]+helper(index+1,1,fee,prices,dp)-fee),helper(index+1,action,fee,prices,dp));
+                }   
+            }
+        }
+        
+        return dp[0][1];
+    }
     
+    /*
+    //recursive and memoized solution
     int helper(int index,int action,int fee,vector<int> &prices,vector<vector<int> > &dp)
     {
         int n=prices.size();
@@ -24,4 +51,5 @@ public:
         vector<vector<int> > dp(n,vector<int>(2,-1));
         return helper(0,1,fee,prices,dp);
     }
+    */
 };
