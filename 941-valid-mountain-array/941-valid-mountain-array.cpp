d68938fc52fa538d26afa7c324eb25c4066139eb
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-09-09 23:22:48 +0530

    Time: 40 ms (66.37%), Space: 22.5 MB (45.77%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.cpp
+++ 941-valid-mountain-array/941-valid-mountain-array.cpp
@@ -1,5 +1,21 @@
 class Solution {
 public:
+    //one pass solution
+    bool validMountainArray(vector<int>& arr) 
+    {
+        if(arr.size()<3) return false;
+        int n=arr.size();
+        int i=0;
+        
+        while(i+1 < n  && arr[i] < arr[i+1]) i++;
+        if(i==n-1 || i==0) return false;
+        while(i+1 < n && arr[i] > arr[i+1]) i++;
+        
+        return i==n-1;
+    }
+    
+    //two pass solution
+    /*
     bool validMountainArray(vector<int>& arr) 
     {
         if(arr.size()<3) return false;
@@ -16,4 +32,5 @@ public:
         if(j==n-2 || i==1) return false;
         return i-1==j+1;
     }
+    */
 };
