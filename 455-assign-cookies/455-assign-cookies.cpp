b319d6d712eb9562d324d1640ac8bb202a1c771f
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-09-20 12:10:32 +0530

    Time: 59 ms (29.50%), Space: 17.5 MB (32.00%) - LeetHub

--- 455-assign-cookies/455-assign-cookies.cpp
+++ 455-assign-cookies/455-assign-cookies.cpp
@@ -5,7 +5,6 @@ public:
         sort(ss.begin(),ss.end());
         int s=0;
         int ans=0;
-        int e=ss.size()-1;
         for(int i=0;i<g.size() && s<ss.size();i++)
         {
             while(s<ss.size() && g[i] > ss[s]){
