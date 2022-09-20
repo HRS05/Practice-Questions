39adb500fe5768a5e91410bef461f04f1f61f729
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-09-20 09:32:01 +0530

    Time: 220 ms (18.53%), Space: 37.9 MB (33.20%) - LeetHub

---
+++ 621-task-scheduler/621-task-scheduler.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        priority_queue<int> pq;
+        queue<pair<int,int> > q;
+        int time=0;
+        int count=0;
+        map<char,int> mp;
+        for(char c : tasks)
+        {
+            mp[c]++;
+        }
+        for(auto x : mp)
+        {
+            pq.push({x.second});
+        }
+        
+        while(!pq.empty() || !q.empty())
+        {
+            time+=1;
+            if(!pq.empty())
+            {
+                if(pq.top()-1 !=0)
+                    q.push({pq.top()-1,time+n});
+                pq.pop();
+            }
+            if(!q.empty() && q.front().second == time)
+            {
+                pq.push({q.front().first});
+                q.pop();
+            }
+        }
+        
+        // while(!pq.empty())
+        // {
+        //     auto x = pq.top(); pq.pop();
+        //     cout<<x.second<<" "<<x.first<<endl;
+        // }
+            
+        return time;
+    }
+};
