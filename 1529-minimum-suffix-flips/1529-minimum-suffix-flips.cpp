ab19ee0291ec74a4d62c8fc0eec5062555b447e0
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-09-21 13:58:00 +0530

    Time: 16 ms (85.17%), Space: 9.6 MB (11.93%) - LeetHub

--- 1529-minimum-suffix-flips/1529-minimum-suffix-flips.cpp
+++ 1529-minimum-suffix-flips/1529-minimum-suffix-flips.cpp
@@ -6,13 +6,10 @@ public:
         int i=n-1;
         while(i>=0 && target[i] == '1') i--;
         if(i!=n-1) count++;
-                    //cout<<"1111count  -> "<<count<<endl;
-
         if(i==-1) return count;
         while(i>=0)
         {
             if(target[i]=='1') count+=2;
-            //cout<<"count  -> "<<count<<endl;
             while(i>=0 && target[i] == '1') i--;
             i--;
         }
