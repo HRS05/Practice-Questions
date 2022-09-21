362919365edf34c3aebb6bf3222ce64d9fed6caf
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-09-21 13:15:25 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (25.00%) - LeetHub

--- 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.cpp
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
     int minimumSum(int num) {
-        string s=to_string(num);
-        sort(s.begin(),s.end());
-        string s1="",s2="";
-        int x1,x2;
-        s1+=s[0];
-        s1+=s[2];
-        s2+=s[1];
-        s2+=s[3];
-        cout<<s1<<"  "<<s2<<endl;
-         x1 = stoi(s1);
-         x2 = stoi(s2);
-        return x1+x2;
+        vector<int> v;
+        int x=10;
+        while(num)
+        {
+            v.push_back(num%x);
+            cout<<num%x<<endl;
+            num=num/x;
+        }
+        sort(v.begin(),v.end());
+        return (v[0]+v[1]) * 10 + (v[2]+v[3]);
     }
+    
+    
+    // int minimumSum(int num) {
+    //     string s=to_string(num);
+    //     sort(s.begin(),s.end());
+    //     string s1="",s2="";
+    //     int x1,x2;
+    //     s1+=s[0];
+    //     s1+=s[2];
+    //     s2+=s[1];
+    //     s2+=s[3];
+    //     cout<<s1<<"  "<<s2<<endl;
+    //      x1 = stoi(s1);
+    //      x2 = stoi(s2);
+    //     return x1+x2;
+    // }
 };
