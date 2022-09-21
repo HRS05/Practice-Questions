9d0a62020097b8ad3acc2ebaddea3e8cbee755bc
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-09-21 14:01:09 +0530

    Time: 15 ms (88.97%), Space: 9.5 MB (45.21%) - LeetHub

--- 1529-minimum-suffix-flips/1529-minimum-suffix-flips.cpp
+++ 1529-minimum-suffix-flips/1529-minimum-suffix-flips.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
+    
     int minFlips(string target) {
+        int ans = 0;
+        for(int i = 1; i < target.size(); i++){
+            if(target[i - 1] != target[i]){
+                ans++;
+            }
+        }
+        if(ans == 0){
+            return 0;
+        }
+        if(target[0] == '0'){
+            return ans;
+        }
+        return ans + 1;
+    }    
+    
+    
+    int minFlipss(string target) {
         int count=0;
         int n=target.size();
         int i=n-1;
