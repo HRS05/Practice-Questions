59151d0929a6dd6e4001706dbae73c733fb3918e
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-09-22 17:28:23 +0530

    Time: 701 ms (46.39%), Space: 8.1 MB (48.28%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    bool f(int index,int i,int j,vector<vector<char>  > &board,string word,vector<vector<int> > &v)
+    bool f(int index,int i,int j,vector<vector<char>  > &board,string word)
     {
         int n=board.size();
         int m=board[0].size();
@@ -14,10 +14,10 @@ public:
         if(index==word.size()-1) return true;
         char c=board[i][j];
         board[i][j]=' ';
-        if(f(index+1,i+1,j,board,word,v)) return true;
-        if(f(index+1,i-1,j,board,word,v)) return true;
-        if(f(index+1,i,j+1,board,word,v)) return true;
-        if(f(index+1,i,j-1,board,word,v)) return true;
+        if(f(index+1,i+1,j,board,word)) return true;
+        if(f(index+1,i-1,j,board,word)) return true;
+        if(f(index+1,i,j+1,board,word)) return true;
+        if(f(index+1,i,j-1,board,word)) return true;
         board[i][j]=c;
         return false;
         
@@ -27,14 +27,13 @@ public:
     bool exist(vector<vector<char>>& board, string word) {
         int n=board.size();
         int m=board[0].size();
-        vector<vector<int> > v(n,vector<int>(m,-1));
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 if(board[i][j]==word[0])
                 {
-                    if(f(0,i,j,board,word,v)) return true;
+                    if(f(0,i,j,board,word)) return true;
                 }
             }
         }
