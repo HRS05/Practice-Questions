7d7df732bfd54386df6cf7ae8edf43db2424d902
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-09-22 17:20:35 +0530

    Time: 1142 ms (18.69%), Space: 8.2 MB (18.21%) - LeetHub

---
+++ 79-word-search/79-word-search.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    
+    bool f(int index,int i,int j,vector<vector<char>  > &board,string word,vector<vector<int> > &v)
+    {
+        int n=board.size();
+        int m=board[0].size();
+        if(i<0 || i>=n || j<0 || j>=m) return false;
+        
+        if(v[i][j]!=-1) return false;
+
+        if(word[index] != board[i][j]) return false;
+        if(index==word.size()-1) return true;
+        
+        v[i][j]=0;
+        if(f(index+1,i+1,j,board,word,v)) return true;
+        if(f(index+1,i-1,j,board,word,v)) return true;
+        if(f(index+1,i,j+1,board,word,v)) return true;
+        if(f(index+1,i,j-1,board,word,v)) return true;
+        v[i][j]=-1;
+        return false;
+        
+    }
+    
+    
+    bool exist(vector<vector<char>>& board, string word) {
+        int n=board.size();
+        int m=board[0].size();
+        vector<vector<int> > v(n,vector<int>(m,-1));
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(board[i][j]==word[0])
+                {
+                    if(f(0,i,j,board,word,v)) return true;
+                }
+            }
+        }
+        return false;
+    }
+};
