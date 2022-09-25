6918bd114070a82522f6892bced784cea59e249c
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-09-25 12:07:43 +0530

    Time: 30 ms (15.92%), Space: 12.2 MB (18.25%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
@@ -1,5 +1,31 @@
 class Solution {
 public:
+    
+    int evalRPN(vector<string>& tokens) {
+        unordered_map<string, function<long (int, int) > > map = {
+            { "+" , [] (int a, int b) { return a + b; } },
+            { "-" , [] (int a, int b) { return a - b; } },
+            { "*" , [] (int a, int b) { return (long)a * b; } },
+            { "/" , [] (int a, int b) { return a / b; } }
+        };
+        std::stack<int> stack;
+        for (string& s : tokens) {
+            if (!map.count(s)) {
+                stack.push(stoi(s));
+            } else {
+                int op1 = stack.top();
+                stack.pop();
+                int op2 = stack.top();
+                stack.pop();
+                stack.push(map[s](op2, op1));
+            }
+        }
+        return stack.top();
+    }
+
+    
+    
+    /*
     bool isOperator(string s)
     {
         return s=="*" || s=="+" || s=="-" || s=="/";
@@ -23,4 +49,5 @@ public:
         }
         return (int)st.top();
     }
+    */
 };
