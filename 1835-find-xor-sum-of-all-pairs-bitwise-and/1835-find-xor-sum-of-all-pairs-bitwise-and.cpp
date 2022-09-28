bd1d4c9af98965f8a2b9ac232b46d38dea29245f
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-09-28 16:07:28 +0530

    Time: 250 ms (55.83%), Space: 93.4 MB (82.06%) - LeetHub

--- 1835-find-xor-sum-of-all-pairs-bitwise-and/1835-find-xor-sum-of-all-pairs-bitwise-and.cpp
+++ 1835-find-xor-sum-of-all-pairs-bitwise-and/1835-find-xor-sum-of-all-pairs-bitwise-and.cpp
@@ -1,5 +1,11 @@
 class Solution {
 public:
+    /*
+    We can observe that (a1 & bi) XOR (a1 & bi) XOR ... XOR (an & bi) is the same as (a1 XOR a2 XOR ... XOR an) & bi.
+
+So, we calculate XOR sum of the first array xa. Then, the final result is (xa & b1) XOR (xa & b2) XOR ... XOR (xa & bm), resulting in a linear solution.
+    */
+    
     int getXORSum(vector<int>& arr1, vector<int>& arr2) {
         int xxor=0;
         int ans=0;
