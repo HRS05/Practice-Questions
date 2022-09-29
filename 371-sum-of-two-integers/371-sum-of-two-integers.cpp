5ca3fc9c7f455def5a699c9945c243d0685df706
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-09-29 13:42:21 +0530

    Time: 4 ms (22.13%), Space: 5.7 MB (94.78%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -2,6 +2,16 @@ class Solution {
 public:
     
     int getSum(int a, int b) {
+        /*
+            a=9
+            b=11
+            a(9) ->      1001
+            b(11) ->     1011
+            a^b -->      0010
+            (a&b)<<1--> 10010 
+            ans = getSum(a^b , ((a&b)<<1))
+        
+        */
         if (b==0) return a;
         int sum = a ^ b;
         int cr = (unsigned int) (a & b) << 1;
