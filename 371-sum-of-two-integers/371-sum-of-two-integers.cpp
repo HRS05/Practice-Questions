32cbc7e53430dc29ee8aa784d90061357f8bba11
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-09-29 12:57:39 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (73.04%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -1,26 +1,41 @@
 class Solution {
 public:
     
-    int getSum(int a, int b) {
-
-    if(b>0)
+        
+    int getSum(int a, int b) 
     {
-         while(b>0)
-        {
-            a++;
-            b--;
-        }  
-    }
-    else
-    {
-        while(b<0)
-        {
-            a--;
-            b++;
+        int c=0;
+        int sum=0;
+        for(int i=0;i<32;i++){
+            int x=(a>>i)&1;
+            int y=(b>>i)&1;
+            sum+=((x^y^c)<<i);
+            c=((x&y)|(c&y)|(c&x));
+            
         }
+        return sum;  
     }
     
-    
+    /*
+    int getSum(int a, int b) 
+    {
+        if(b>0)
+        {
+             while(b>0)
+            {
+                a++;
+                b--;
+            }  
+        }
+        else
+        {
+            while(b<0)
+            {
+                a--;
+                b++;
+            }
+        }
     return a;
-}
+    }
+    */
 };
