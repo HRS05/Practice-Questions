10adaf416f0fd73a3d720f7fdb37fb22a4460605
Harsh Sharma <64305522+HRS05@users.noreply.github.com>
2022-09-29 17:01:50 +0530

    Time: 60 ms (6.61%), Space: 13.9 MB (75.06%) - LeetHub

--- 393-utf-8-validation/393-utf-8-validation.cpp
+++ 393-utf-8-validation/393-utf-8-validation.cpp
@@ -1,47 +1,58 @@
 class Solution {
 public:
-    bool validUtf8(vector<int>& data) {
-        int n = data.size();
-        
-        /*
-        
-         128 64 32 16 8 4 2 1
-         1    1  1  1 0 0 0 0 -> 240
-         1    1  1  0 0 0 0 0 -> 224
-         1    1  0  0 0 0 0 0 -> 192
-         1    0  0  0 0 0 0 0 -> 128
-        
-        
-        
-        */
-        for(int i=0;i<n;i++){
-            int bytes = 0;
-            int val = data[i];
-            if(val > 255){
-                return false;
-            }else if((val & 128) == 0){
-                bytes = 1;
-            }else if((val & 224) == 192){ // val bit anded with 224 will give 192  
-                bytes = 2;
-                
-            }else if((val & 240) == 224){
-                bytes = 3;
-                
-            }else if((val & 248) == 240){
-                bytes = 4;
-            }else{
-                return false;
+    bool validUtf8(vector<int>& data) 
+    {
+        int n=data.size();
+        for(int i=0;i<n;i++)
+        {
+            if((data[i]>>7) == 0) 
+            {
+                cout<<data[i]<<endl;
+                continue;
             }
-            
-            for(int j=1;j<bytes;j++){
-                if(j+i >= data.size()){ // if found a 3 degree , the utf set should have 111xxxx 10xxxxxx 10xxxxxx
-                    // if the bytes + index at which you are in exceeds the data size means theres incomplete UTF set so return false;
-                    return false;
-                }else if((data[j+i]&192) != 128){ // if fouund any n degree the next elements should have n-1 10xxxxxx(128) elements
-                    return false;
+            else if((data[i]>>5) == 6)
+            {
+                cout<<data[i]<<endl;
+                int j=i+1;
+                while(j<=i+1 && j<n)
+                {
+                    cout<<(data[j]>>6)<<endl;
+                    if((data[j]>>6) != 2) return false;
+                    j++;
                 }
+                cout<<j<<"  "<<i+2<<endl;
+                if(j!=i+2) return false;
+                i=j-1;
             }
-            i+=bytes-1; // UTF set will contain Bytes terms , so to jump on next UTF set (bytes -1)
+            else if((data[i]>>4) == 14)
+            {
+                int j=i+1;
+                while(j<=i+2 && j<n)
+                {
+                    if((data[j]>>6) != 2) return false;
+                    j++;
+                }
+                if(j!=i+3) return false;
+                i=j-1;
+            }
+            else if((data[i]>>3) == 30)
+            {
+                cout<<data[i]<<endl;
+                int j=i+1;
+                while(j<=i+3 && j<n)
+                {
+                    if((data[j]>>6) != 2) return false;
+                    j++;
+                }
+                if(j!=i+4) return false;
+                i=j-1;
+            }
+            else
+            {
+                //cout<<data[i]<<endl;
+                cout<<"hello"<<endl;
+                 return false;
+            }
         }
         return true;
     }
